5af0e1495746f757eb9e2e5e99f225db133541f8
vitaliy <vitaliynif@gmail.com>
2022-12-09 14:13:36 +0700

    add before all to tests

--- src/js/index.test.js
+++ src/js/index.test.js
@@ -1,23 +1,29 @@
 const TreeStore = require('./index.js');
 
-const items = [
-    { id: 1, parent: 'root' },
-    { id: 2, parent: 1, type: 'test' },
-    { id: 3, parent: 1, type: 'test' },
 
-    { id: 4, parent: 2, type: 'test' },
-    { id: 5, parent: 2, type: 'test' },
-    { id: 6, parent: 2, type: 'test' },
-
-    { id: 7, parent: 4, type: null },
-    { id: 8, parent: 4, type: null },
-];
-
-const res = new TreeStore(items);
 
 console.log('TreeStore tests');
 
 describe('TreeStoreTests', () => {
+    let res;
+    let items;
+    beforeAll(() => {
+        items = [
+            { id: 1, parent: 'root' },
+            { id: 2, parent: 1, type: 'test' },
+            { id: 3, parent: 1, type: 'test' },
+
+            { id: 4, parent: 2, type: 'test' },
+            { id: 5, parent: 2, type: 'test' },
+            { id: 6, parent: 2, type: 'test' },
+
+            { id: 7, parent: 4, type: null },
+            { id: 8, parent: 4, type: null },
+        ];
+
+        res = new TreeStore(items);
+    })
+
     test('validateGetAll', () => {
         expect(res.getAll()).toEqual(items);
     });
@@ -28,16 +34,16 @@ describe('TreeStoreTests', () => {
         expect(res.getChildren(4)).toEqual([{ id: 7, parent: 4, type: null }, { id: 8, parent: 4, type: null }]);
     });
     test('validateGetChildrenWithExisted2', () => {
-        expect(res.getChildren(2)).toEqual([{"id":4,"parent":2,"type":"test"},{"id":5,"parent":2,"type":"test"},{"id":6,"parent":2,"type":"test"}]);
+        expect(res.getChildren(2)).toEqual([{ "id": 4, "parent": 2, "type": "test" }, { "id": 5, "parent": 2, "type": "test" }, { "id": 6, "parent": 2, "type": "test" }]);
     })
     test('validateGetChildrenWithoutChildren', () => {
         expect(res.getChildren(5)).toEqual([]);
     });
     test('validateGetAllChildren', () => {
-        expect(res.getAllChildren(2)).toEqual([{"id":4,"parent":2,"type":"test"},{"id":5,"parent":2,"type":"test"},{"id":6,"parent":2,"type":"test"},{"id":7,"parent":4,"type":null},{"id":8,"parent":4,"type":null}]);
+        expect(res.getAllChildren(2)).toEqual([{ "id": 4, "parent": 2, "type": "test" }, { "id": 5, "parent": 2, "type": "test" }, { "id": 6, "parent": 2, "type": "test" }, { "id": 7, "parent": 4, "type": null }, { "id": 8, "parent": 4, "type": null }]);
     });
     test('validateGetAllParents', () => {
-        expect(res.getAllParents(7)).toEqual([{"id":4,"parent":2,"type":"test"},{"id":2,"parent":1,"type":"test"},{"id":1,"parent":"root"}]);
+        expect(res.getAllParents(7)).toEqual([{ "id": 4, "parent": 2, "type": "test" }, { "id": 2, "parent": 1, "type": "test" }, { "id": 1, "parent": "root" }]);
     });
 })
 
